653662365f1213fda0add20476602023e2666504
michael-prye <michael.prye@gmail.com>
2022-03-23 18:04:27 -0400

    Added single player method + get winner method

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
-
+from human import Human
+from ai import Ai
 class Game:
     def __init__(self):
         pass
@@ -24,8 +25,53 @@ class Game:
     def display_rules(self):
         print('Rock crushes Scissors\nScissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper')
     def single_player(self):
-        pass
+        p1_wins = 0
+        ai_wins = 0
+        rounds = 0
+        player_one = Human()
+        player_ai = Ai()
+
+        while rounds <=3 or p1_wins == 2 or ai_wins == 2:
+            p1_gesture = player_one.my_gesture()
+            ai_gesture = player_ai.ai_gesture()
+            x = self.get_winner(p1_gesture, ai_gesture)
+            if x == 1:
+                p1_wins += 1
+                rounds += 1
+            elif x == 2:
+                ai_wins += 1
+                rounds += 1
+            else:
+                round += 1 
     def multiplayer(self):
         pass
+    def get_winner(self, p1,p2):
+        if p1 == p2:
+            return 0
+        if p1 == 'rock' and p2 == 'scissors' or p2 == 'lizard':
+            return 1
+        elif p1 == 'scissors' and p2 == 'paper' or p2 == 'lizard':
+            return 1
+        elif p1 == 'paper' and p2 == 'rock' or p2 == 'spock':
+            return 1
+        elif p1 == 'lizard' and p2 == 'spock' or p2 == 'paper':
+            return 1
+        elif p1 == 'spock' and p2 == 'scissors' or p2 == 'rock':
+            return 1
+        else:
+            return 2
+        
+
 test = Game()
-test.run_game()
+print(test.get_winner('rock', 'lizard'))
+
+# Rock crushes Scissors 
+# Rock crushes Lizard 
+# Scissors cuts Paper 
+# Scissors decapitates Lizard 
+# Paper covers Rock 
+# Paper disproves Spock 
+# Lizard poisons Spock 
+# Lizard eats Paper
+# Spock smashes Scissors 
+# Spock vaporizes Rock
